cf76953bc6ecb3038ab28d65499f271bb0835372
Sprayer1122 <sprayiton1122@gmail.com>
2025-03-16 19:22:32 +0530

    added comment

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask!" #this is the new era
 
 if __name__ == '__main__':
     app.run(debug=True)
